26195e8b2d03e52b30e81ec326cac31037c588ad
ansango <anibalsantosgo@gmail.com>
2022-01-10 23:42:14 +0100

    config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,14 +7,14 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "ansango docs",
-  tagline: "Dev Docs are cool",
-  url: "https://your-docusaurus-test-site.com",
+  tagline: "Dev Docs molan",
+  url: "https://ansango-devdocs.vercel.app/",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
-  organizationName: "ansango", // Usually your GitHub org/user name.
-  projectName: "dev docs", // Usually your repo name.
+  organizationName: "ansango",
+  projectName: "dev docs",
   i18n: {
     defaultLocale: "es",
     locales: ["es"],
@@ -50,8 +50,7 @@ const config = {
           routeBasePath: "/",
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/ansango/devdocs/edit/main/",
         },
         blog: false,
         theme: {
